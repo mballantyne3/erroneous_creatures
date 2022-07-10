630afde778874dff94f0c80058d2f80bd8ddf764
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-10 12:10:20 -0600

    Fix wizard bugs

--- lib/wizard.rb
+++ lib/wizard.rb
@@ -3,13 +3,13 @@ class Wizard
   attr_reader :name
   attr_accessor :bearded
 
-  def initialize(name, bearded = {bearded: true})
+  def initialize(name, bearded = true)
     @name = name
     @bearded = bearded
   end
 
   def bearded?
-    @bearded
+    @bearded == true
   end
 
   def incantation(x)
